2fd67911cf4b229d0e332109e768a596c5cdb206
mf-pc-home <mengfan.1993@163.com>
2022-11-19 21:27:48 +0800

    1119-21:30-更新一下

--- pytroch/论文复现/Informer/Informer2020-main/main_informer.py
+++ pytroch/论文复现/Informer/Informer2020-main/main_informer.py
@@ -48,14 +48,14 @@ parser.add_argument('--embed', type=str, default='timeF', help='time features en
                                                                '[timeF, fixed, learned]')
 parser.add_argument('--activation', type=str, default='gelu',help='activation')
 parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
-parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+parser.add_argument('--do_predict', action='store_true', default=True, help='whether to predict unseen future data')
 # mf-mix没有特别的解释
 parser.add_argument('--mix', action='store_false', help='use mix attention in generative decoder', default=True)
 parser.add_argument('--cols', type=str, nargs='+', help='certain cols from the data files as the input features')
 # mf-window必须设置为0
 parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
-parser.add_argument('--itr', type=int, default=2, help='experiments times')
-parser.add_argument('--train_epochs', type=int, default=6, help='train epochs')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')  #  默认值为2
+parser.add_argument('--train_epochs', type=int, default=3, help='train epochs')  # 默认值为6
 parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
 parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
 parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
@@ -63,7 +63,7 @@ parser.add_argument('--des', type=str, default='test',help='exp description')
 parser.add_argument('--loss', type=str, default='mse',help='loss function')  # mf-损失函数
 parser.add_argument('--lradj', type=str, default='type1',help='adjust learning rate')
 parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
-parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
+parser.add_argument('--inverse', action='store_true', help='inverse output data', default=True)  # 默认值为False
 
 parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
 parser.add_argument('--gpu', type=int, default=0, help='gpu')
@@ -89,10 +89,9 @@ data_parser = {
     'WTH':{'data':'WTH.csv','T':'WetBulbCelsius','M':[12,12,12],'S':[1,1,1],'MS':[12,12,1]},
     'ECL':{'data':'ECL.csv','T':'MT_320','M':[321,321,321],'S':[1,1,1],'MS':[321,321,1]},
     'Solar':{'data':'solar_AL.csv','T':'POWER_136','M':[137,137,137],'S':[1,1,1],'MS':[137,137,1]},
-    'WTH_small': {'data': 'WTH_small.csv', 'T': 'WetBulbCelsius', 'M': [4, 4, 4], 'S': [1, 1, 1], 'MS': [4, 4, 1],
-                  'itr': 1, 'train_epochs': 3, 'do_predict': True, '--inverse': True},
-    'my_data': {'data': 'my_data.csv', 'T': 'AC', 'M': [4, 4, 4], 'S': [1, 1, 1], 'MS': [4, 4, 1],
-                'do_predict': True},
+    'WTH_small': {'data': 'WTH_small.csv', 'T': 'WetBulbCelsius', 'M': [4, 4, 4], 'S': [1, 1, 1], 'MS': [4, 4, 1],},
+    # 'itr': 1, 'train_epochs': 3, 'do_predict': True, '--inverse': True
+    'my_data': {'data': 'my_data.csv', 'T': 'AC', 'M': [4, 4, 4], 'S': [1, 1, 1], 'MS': [4, 4, 1]},
 }
 if args.data in data_parser.keys():
     data_info = data_parser[args.data]
